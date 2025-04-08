fc3ade52d9c3a8c7b0eccc7c31ab6e7a6d00305b
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2025-04-08 18:52:06 +0700

    feat: updated environment

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
     reactStrictMode: false,
     env:{
-        BASE_URL:process.env.BASE_URL,
-        BASE_URL_DEV:process.env.BASE_URL_DEV
+        NEXT_PUBLIC_BASE_URL:process.env.NEXT_PUBLIC_BASE_URL,
+        NEXT_PUBLIC_BASE_URL_DEV:process.env.NEXT_PUBLIC_BASE_URL_DEV
     },
 }
 export default nextConfig
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === 'development' ? process.env.BASE_URL_DEV  : process.env.BASE_URL;
+export const BASE_URL = process.env.NODE_ENV === 'development' ? process.env.NEXT_PUBLIC_BASE_URL_DEV  : process.env.NEXT_PUBLIC_BASE_URL ;
