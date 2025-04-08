1f51c9600dee9096ea681e9c52d8a74a341bdb01
MuhammadAliffandy <aliffandyrealgo@gmail.com>
2025-04-08 17:19:03 +0700

    feat: update routing push on quick

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
 'use client'
 
+import { Tooltip } from "@mui/material";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router  = useRouter()
   return (
     <div className="w-full h-[100vh] bg-[#0F8A69] flex items-center justify-center">
-      <p className="text-[200px] leading-[200px] text-white text-center font-bold">Simple<br/>Quicks</p>
+        <Tooltip title ="Click, to show quicks">
+          <p onClick={()=>{router.push('/quick')}} className="text-[200px] leading-[200px] text-white text-center font-bold cursor-pointer">Simple<br/>Quicks</p>
+        </Tooltip>
     </div>
   );
 }
